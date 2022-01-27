828bfb4b7174894c81dada7fa84bd7d91250df67
Rana  Sagar <64120304+sagar-demo@users.noreply.github.com>
2022-01-27 06:01:01 -0500

    Add files via upload

---
+++ a.cpp
@@ -0,0 +1,10 @@
+#include<iostream> //Header file
+using namespace std;//if we use this header file ,then we don't mention std:: in  cout<<
+int main()
+{
+	int a=8;
+	int b=9;
+//	std::cout<<"Hello World!"; //Here std in function inside namespace function
+    cout<<a;cout<<b; 
+	return 0;
+}
