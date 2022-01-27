423022e09a917453c60a286230b538884136a63b
Rana  Sagar <64120304+sagar-demo@users.noreply.github.com>
2022-01-27 09:09:54 -0500

    Add files via upload

---
+++ inputoutput.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a,b,mul1,mul2;
+	cout<<"Enter the value of A:\n";
+	cin>>a;
+	cout<<"Enter the value of B:\n";
+	cin>>b;
+	int c=a+b;
+//	cout<<"The sum of 2 No is:"<< c;
+	cout<<"\nEnter the value of A:\n";
+	cin>>mul1;
+	cout<<"Enter the value of B:\n";
+	cin>>mul2;
+	int d=mul1*mul2;
+	cout<<"The Multiply of 2 No is:"<< d;
+	cout<<"\nThe sum of 2 No is:"<< c;
+	
+//	cout<<"The sum of two no is: "<< a+b;
+	return 0;
+}
