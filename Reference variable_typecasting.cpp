c8aacaeed138537f4d6ef2b6e625aea51c8577fc
Rana  Sagar <64120304+sagar-demo@users.noreply.github.com>
2022-01-29 04:59:28 -0500

    Add files via upload

---
+++ Reference variable_typecasting.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+//int c=12;//global variable
+
+int main()
+{
+//	int a,b,c,x;
+//	
+//	cout<<"Enter the value of A: "<<endl;
+//	cin>>a;
+//	cout<<"Enter the value of B: "<<endl;
+//	cin>>b;
+//	c=a+b;
+//	cout<<"Sum of two no is: "<<c<<endl;
+//	x=::c+a;// here we use scope resolution to access of global variable value
+//	cout<<"the global variable C value is: "<<x;
+
+//******** Reference Variables******** we use this symbol -&-****
+
+//	float a=12.2L;
+//	float &y=a;
+//	cout<<"Value of float a: "<<a<<endl;
+//	cout<<"Value of float a: "<<a<<endl;
+//	cout<<"Value of float a: "<<sizeof(a)<<endl;
+
+//TypeCasting**********
+	float a=10.1;
+	cout<<"The float value of A is: "<<int(a)<<endl;// another way of type casting
+//	int c=float(a);//typecasting
+//	cout<<c;
+	return 0;
+}
