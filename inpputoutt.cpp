1296a1b24acdea62d51fc75c90af4d504b252705
Rana  Sagar <64120304+sagar-demo@users.noreply.github.com>
2022-02-19 10:14:26 -0500

    Add files via upload

---
+++ inpputoutt.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int num1,num2;
+int main()
+{
+	cout<<"Enter First Value:\n";
+	cin>>num1;
+	cout<<"Enter Second Value:\n";
+	cin>>num2;
+	cout<<"Sum of two no is:"<<"\n"<<num1+num2;
+	
+	return 0;
+}
