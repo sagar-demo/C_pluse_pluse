275f28dc7286fd16281114e1115fa5432807207a
Rana  Sagar <64120304+sagar-demo@users.noreply.github.com>
2022-02-20 08:56:49 -0500

    Add files via upload

---
+++ Referencevar.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+//	float d=34.f;
+//	long double e=34.4L;
+//	cout<<"The size of 34.4 is "<<sizeof(34.4)<<endl;
+//	cout<<"The size of 34.4 is "<<sizeof(34.4f)<<endl;
+//	cout<<"The size of 34.4 is "<<sizeof(34.4L)<<endl;
+
+//**** Reference variable
+
+int x=2;
+int &y=x; // reference variable
+cout<<x<<endl;
+cout<<y<<endl;
+
+
+	return 0;
+}
---
+++ constMaipulators.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	 int a=10;
+	cout<<"The value of a was: "<<a<<endl;
+	a=20;
+	cout<<"The value of a is: "<<a;                                                       
+	return 0;
+	
+}
