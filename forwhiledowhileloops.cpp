37124e9c439fe2014c98aa15a3be8d0b68e331ee
Rana  Sagar <64120304+sagar-demo@users.noreply.github.com>
2022-02-25 06:52:40 -0500

    Add files via upload

---
+++ forwhiledowhileloops.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+// For loop
+	
+//	for(int i=1;0<1;i++) infinite for loop it is
+//	{
+//		cout<<i<<endl;
+//	}
+
+// while loop infinite
+
+//char i;
+//while(true)
+//{
+//	cout<<"Hello Good Morning:"<<endl;
+//	i++;
+//}
+
+//int i=1;
+//while(i<=100)
+//{
+//	cout<<i<<endl;
+//	i++;
+//}
+
+int i=6;
+while(i<=60)
+{
+	cout<<i<<endl;
+	i++;
+}
+
+
+	return 0;
+}
---
+++ table6using.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+// By using for loops
+
+//	int j=6;
+//	for (int i=1;i<=10;i++)
+//	{
+//		cout<<j<<" * "<<i<<" = "<<j*i<<endl;
+//	}
+
+//By using while loops
+
+int i=1;
+//while(i<=10)
+//{
+//	cout<<"6"<<"*"<<i<<"="<<i*6<<endl;
+//	i++;
+//}
+do
+{
+	cout<<i*6<<endl;
+	i++;
+}
+while(i<11);
+	return 0;
+}
